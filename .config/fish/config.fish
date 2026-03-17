0876ac5bb17d897d7f00ea65cab1ad86b0336fd0
onicuto <kiryauvachan@gmail.com>
2026-03-17 16:05:02 +0000

    redact for void

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,18 +1,11 @@
 if status is-interactive
-    export LFS=/mnt/lfs
     set -U fish_greeting ""
     fastfetch
     alias py="python3"
     set -xg EDITOR /usr/bin/nvim
     alias vim="nvim"
-    alias telefon="scrcpy"
-    alias lua="lua5.1"
     alias fishconf="nvim /home/onicuto/.dotfiles/.config/fish/config.fish"
-    alias mirrors="sudo reflector --country Russia --age 12 --protocol https --sort rate --save /etc/pacman.d/mirrorlist"
     alias conf="nvim ~/.dotfiles/.config"
-    alias archfetch="brl strat arch ufetch"
-    alias voidfetch="brl strat void ufetch"
-    alias debfetch="brl strat debian ufetch"
     alias lsblk="lsblk -o NAME,SIZE,TYPE,FSTYPE,MOUNTPOINT"
     # Commands to run in interactive sessions can go here
 end
--- .config/nvim/lua/plugins/example.lua
+++ .config/nvim/lua/plugins/example.lua
@@ -223,7 +223,7 @@ return {
       -- You can choose one of the following pickers
       "nvim-telescope/telescope.nvim",
       "ibhagwan/fzf-lua",
-      "echasnovski/mini.pick",
+      "nvim-mini/mini.pick",
       "folke/snacks.nvim",
     },
   },
