b19864185e36430612845950c6892e63c97d677f
onicuto <kiryauvachan@gmail.com>
2026-04-02 15:28:29 +0300

    change nvim .conf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,6 @@
 if status is-interactive
     export LFS=/mnt/lfs
+    export FLSH=/run/media/onicuto
     set -U fish_greeting ""
     fastfetch
     alias py="python3"
@@ -7,6 +8,8 @@ if status is-interactive
     alias vim="nvim"
     alias telefon="scrcpy"
     alias lua="lua5.1"
+    alias vpip="source ~/.venv/bin/activate.fish"
+    alias vpip_exit="deactivate"
     alias fishconf="nvim /home/onicuto/.dotfiles/.config/fish/config.fish"
     alias mirrors="sudo reflector --country Russia --age 12 --protocol https --sort rate --save /etc/pacman.d/mirrorlist"
     alias conf="nvim ~/.dotfiles/.config"
@@ -14,9 +17,14 @@ if status is-interactive
     alias voidfetch="brl strat void ufetch"
     alias debfetch="brl strat debian ufetch"
     alias lsblk="lsblk -o NAME,SIZE,TYPE,FSTYPE,MOUNTPOINT"
+    alias brlconf="sudoedit /bedrock/etc/bedrock.conf"
     # Commands to run in interactive sessions can go here
 end
 
 if test (tty) = /dev/tty1
     exec start-hyprland >/dev/null 2>&1
+end
+
+if test (tty) = /dev/tty2
+    startx
 end
