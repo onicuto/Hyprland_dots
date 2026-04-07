fcfb8c6a8454877a7e9910ffb64db349a6a067e3
onicuto <kiryauvachan@gmail.com>
2026-04-07 21:37:15 +0300

    del icons

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,35 +1,47 @@
-return {
-  -- add gruvbox
-  { "RRethy/nvim-base16" },
+-- return {
+--   -- add gruvbox
+--
+--
+--   -- Configure LazyVim to load gruvbox
+--   {
+--     "LazyVim/LazyVim",
+--     init = function()
+--       local clear_background = function()
+--         local groups = {
+--           "Normal",
+--           "NormalNC",
+--           "EndOfBuffer",
+--           "SignColumn",
+--           "LineNr",
+--           "FoldColumn",
+--           "CursorLineNr",
+--           "NormalFloat",
+--           "FloatBorder",
+--         }
+--
+--         for _, group in ipairs(groups) do
+--           vim.api.nvim_set_hl(0, group, { bg = "none" })
+--         end
+--       end
+--
+--       vim.api.nvim_create_autocmd("ColorScheme", {
+--         callback = clear_background,
+--       })
+--
+--       vim.schedule(clear_background)
+--     end,
+--     opts = {
+--       colorscheme = "base16-grayscale-dark",
+--     },
+--   },
+-- }
 
-  -- Configure LazyVim to load gruvbox
+return {
+  { "typicode/bg.nvim", lazy = false },
+  { "nyoom-engineering/oxocarbon.nvim" },
+  { "RRethy/nvim-base16" },
   {
     "LazyVim/LazyVim",
-    init = function()
-      local clear_background = function()
-        local groups = {
-          "Normal",
-          "NormalNC",
-          "EndOfBuffer",
-          "SignColumn",
-          "LineNr",
-          "FoldColumn",
-          "CursorLineNr",
-          "NormalFloat",
-          "FloatBorder",
-        }
-
-        for _, group in ipairs(groups) do
-          vim.api.nvim_set_hl(0, group, { bg = "none" })
-        end
-      end
-
-      vim.api.nvim_create_autocmd("ColorScheme", {
-        callback = clear_background,
-      })
-
-      vim.schedule(clear_background)
-    end,
     opts = {
       colorscheme = "base16-grayscale-dark",
     },
--- .config/nvim/lua/plugins/live.lua
+++ .config/nvim/lua/plugins/live.lua
@@ -4,7 +4,7 @@ return {
     -- You can choose one of the following pickers
     "nvim-telescope/telescope.nvim",
     "ibhagwan/fzf-lua",
-    "mini-pick/mini.pick",
+    "nvim-mini/mini.pick",
     "folke/snacks.nvim",
   },
 }
