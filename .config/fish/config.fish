c2e4e8e0219d73e17bc07db247c597005cb05341
onicuto <kiryauvachan@gmail.com>
2026-06-15 21:32:15 +0300

    redact sensivity

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -24,3 +24,10 @@ end
 if test (tty) = /dev/tty2
     startx
 end
+
+# pnpm
+set -gx PNPM_HOME "/home/onicuto/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
